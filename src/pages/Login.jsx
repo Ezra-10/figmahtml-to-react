8079618f018bba0edb78bd274df012cd6f4338fd
ZraaEzra <85428644+Ezra-10@users.noreply.github.com>
2025-02-07 14:02:52 +0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -42,7 +42,7 @@ const Register = () => {
           /><br />
           
           <br />
-          <button className="btn" type="submit"><a href="/home">Submit</a></button>
+          <Link to="/Home">submit</Link>
         </form>
         <p style={{ textAlign: 'center' }}>
           Dont have an account? <Link to="/register">Come Here</Link>
