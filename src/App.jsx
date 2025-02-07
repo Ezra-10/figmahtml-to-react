28ac2b20f2834fc18f949a182d4317afa14f0e76
ZraaEzra <85428644+Ezra-10@users.noreply.github.com>
2025-02-07 14:01:16 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/home' element={<Home/>}/>
+        <Route path='/Home' element={<Home/>}/>
         <Route path='/' element={<Login/>}/>
         <Route path='/Register' element={<Register/>}/>
       </Routes>
